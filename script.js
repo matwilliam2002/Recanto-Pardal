3d70178b42b01594bc7c28e982a00dd4090fd554
Mateus William <mateuswrcunha@gmail.com>
2024-06-17 21:23:04 -0300

    Atualização da página

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
+document.querySelectorAll('.galeria img').forEach(Image=>{
+  Image.onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'block';
+    document.querySelector('.popup-image img').src = Image.getAttribute('src');
+  }
+});
 
-let currentSlide = 0;
+document.querySelector('.popup-image span').onclick=() =>{
+  document.querySelector('.popup-image').style.display = 'none';
 
-function nextSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('active');
 }
-
-function prevSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  slides[currentSlide].classList.add('active');
-}
-
-slider.addEventListener('swipeleft', nextSlide);
-slider.addEventListener('swiperight', prevSlide);
-
-// Opcional: navegação com setas (crie elementos HTML para as setas)
-const nextBtn = document.querySelector('.slider-nav .next');
-const prevBtn = document.querySelector('.slider-nav .prev');
-
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);

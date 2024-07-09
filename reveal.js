9dca08050ca4f3a0e08e16ff6ab2b0e1ee94fd93
Mateus William <mateuswrcunha@gmail.com>
2024-07-08 21:00:27 -0300

    Finalizações 1

---
+++ reveal.js
@@ -0,0 +1,34 @@
+window.sr =  ScrollReveal({reset:true});
+sr.reveal('.navbar', 
+    {
+        duration: 2000
+    });
+sr.reveal('.especificações', 
+    {
+        duration: 1000,
+        distance:'90px'
+    });
+sr.reveal('.galeria', 
+    {
+        duration: 1500,
+        distance:'90px'
+    })
+
+sr.reveal('.chamada', 
+    {
+        duration: 1500,
+        distance:'90px'
+    })
+
+sr.reveal('.mais-info', 
+        {
+            duration: 1500,
+            distance:'90px'
+        })
+    
+sr.reveal('.localizacao', 
+            {
+                duration: 1500,
+                distance:'90px'
+            })
+        
--- script.js
+++ script.js
@@ -1,106 +1,104 @@
 document.querySelectorAll('.galeria img').forEach(image => {
-  image.onclick = () => {
-      document.querySelector('.popup-image').style.display = 'block';
-      document.querySelector('.popup-image img').src = image.getAttribute('src');
-  }
+    image.onclick = () => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-content img').src = image.getAttribute('src');
+    }
 });
 
 document.querySelector('.popup-image span').onclick = () => {
-  document.querySelector('.popup-image').style.display = 'none';
-}
+    document.querySelector('.popup-image').style.display = 'none';
+};
 
 function initMap() {
-  var center = { lat: -23.196109, lng: -46.803599 };
-
-
-  var mapOptions = {
-      center: center,
-      zoom: 17
-  };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: center,
-    map: map,
-    title: 'Recanto do Pardal'
-});
+    var center = { lat: -23.196109, lng: -46.803599 };
+    var mapOptions = {
+        center: center,
+        zoom: 17
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Recanto do Pardal'
+    });
 }
 
-document.addEventListener('DOMContentLoaded', initMap);
+document.addEventListener('DOMContentLoaded', function() {
+    initMap();
 
-(function(){
-  emailjs.init("RRFNdtNEG4R1PN24P"); 
-})();
+    (function(){
+        emailjs.init("RRFNdtNEG4R1PN24P"); 
+    })();
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-  const serviceID = 'service_fd6ri09';
-  const templateID = 'template_awulm8g'; 
+        const serviceID = 'service_fd6ri09';
+        const templateID = 'template_awulm8g'; 
 
-  emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-          alert('Email enviado com sucesso!');
-          this.reset();
-      }, (err) => {
-          alert('Erro ao enviar o email. Por favor, tente novamente.');
-      });
-});
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                alert('Email enviado com sucesso!');
+                this.reset();
+            }, (err) => {
+                alert('Erro ao enviar o email. Por favor, tente novamente.');
+            });
+    });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const popupImage = document.querySelector(".popup-image");
-  const popupImgElement = document.querySelector(".popup-content img");
-  const popupVideoElement = document.querySelector(".popup-content video");
-  const closeBtn = document.querySelector(".popup-image span");
-  const prevBtn = document.querySelector(".prev");
-  const nextBtn = document.querySelector(".next");
-  const mediaItems = document.querySelectorAll(".imagem-container img, .imagem-container video");
+    const popupImage = document.querySelector(".popup-image");
+    const popupImgElement = document.querySelector(".popup-content img");
+    const popupVideoElement = document.querySelector(".popup-content video");
+    const closeBtn = document.querySelector(".popup-image span");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+    const mediaItems = document.querySelectorAll(".imagem-container img, .imagem-container video");
 
-  let currentIndex = 0;
+    let currentIndex = 0;
 
-  function openPopup(index) {
-      currentIndex = index;
-      const currentItem = mediaItems[currentIndex];
-      if (currentItem.tagName.toLowerCase() === "img") {
-          popupImgElement.src = currentItem.src;
-          popupImgElement.style.display = "block";
-          popupVideoElement.style.display = "none";
-      } else if (currentItem.tagName.toLowerCase() === "video") {
-          popupVideoElement.querySelector("source").src = currentItem.querySelector("source").src;
-          popupVideoElement.load();
-          popupVideoElement.style.display = "block";
-          popupImgElement.style.display = "none";
-      }
-      popupImage.style.display = "flex";
-  }
+    function openPopup(index) {
+        currentIndex = index;
+        const currentItem = mediaItems[currentIndex];
+        if (currentItem.tagName.toLowerCase() === "img") {
+            popupImgElement.src = currentItem.src;
+            popupImgElement.style.display = "block";
+            popupVideoElement.style.display = "none";
+            popupVideoElement.pause(); // Pausa o vídeo, se estiver reproduzindo
+        } else if (currentItem.tagName.toLowerCase() === "video") {
+            popupVideoElement.querySelector("source").src = currentItem.querySelector("source").src;
+            popupVideoElement.load();
+            popupVideoElement.style.display = "block";
+            popupImgElement.style.display = "none";
+        }
+        popupImage.style.display = "flex";
+    }
 
-  function closePopup() {
-      popupImage.style.display = "none";
-      popupVideoElement.pause();
-  }
+    function closePopup() {
+        popupImage.style.display = "none";
+        popupVideoElement.pause(); // Pausa o vídeo ao fechar o popup
+    }
 
-  function showPrevItem() {
-      currentIndex = (currentIndex > 0) ? currentIndex - 1 : mediaItems.length - 1;
-      openPopup(currentIndex);
-  }
+    function showPrevItem() {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : mediaItems.length - 1;
+        openPopup(currentIndex);
+    }
 
-  function showNextItem() {
-      currentIndex = (currentIndex < mediaItems.length - 1) ? currentIndex + 1 : 0;
-      openPopup(currentIndex);
-  }
+    function showNextItem() {
+        currentIndex = (currentIndex < mediaItems.length - 1) ? currentIndex + 1 : 0;
+        openPopup(currentIndex);
+    }
 
-  mediaItems.forEach((item, index) => {
-      item.addEventListener("click", () => openPopup(index));
-  });
+    mediaItems.forEach((item, index) => {
+        item.addEventListener("click", () => openPopup(index));
+    });
 
-  closeBtn.addEventListener("click", closePopup);
-  prevBtn.addEventListener("click", showPrevItem);
-  nextBtn.addEventListener("click", showNextItem);
+    closeBtn.addEventListener("click", closePopup);
+    prevBtn.addEventListener("click", showPrevItem);
+    nextBtn.addEventListener("click", showNextItem);
 
-  popupImage.addEventListener("click", function(event) {
-      if (event.target === popupImage) {
-          closePopup();
-      }
-  });
+    popupImage.addEventListener("click", function(event) {
+        if (event.target === popupImage) {
+            closePopup();
+        }
+    });
 });
 
